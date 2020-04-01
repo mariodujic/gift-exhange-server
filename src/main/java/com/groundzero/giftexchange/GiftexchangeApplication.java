31503799e7f3d193151c0344adf5eeff80dc1be4
Mario Dujić <liberasoftware@gmail.com>
2020-04-01 14:59:04 +0200

    init

---
+++ src/main/java/com/groundzero/giftexchange/GiftexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.groundzero.giftexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftexchangeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GiftexchangeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/groundzero/giftexchange/GiftexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groundzero.giftexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftexchangeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
